c9951400cbbc3dea269c4c0bd61b276dc1a67c54
Sheela Ricafrente <ricafrente.sheela@gmail.com>
2023-04-27 00:51:14 +0800

    First commit

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,17 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+    """Determine if the list of favorite numbers match
+    
+    :param fave_numbers_1: list - first list of favorite numbers
+    :param fave_numbers_2: list - second list of favorite numbers
+    :return: bool -  do the first set of favorite numbers contain the second
+                    set of favorite numbers or vice versa?
+    """
+    #Converting lists to set to make sure every member is unique
+    set_1 = set(fave_numbers_1)
+    set_2 = set(fave_numbers_2)
 
-    return True
+    #If the first set contains the the whole second set or vice versa, 
+    #return True. If both conditions doesn't apply, return false
+    return set_1.issuperset(set_2) or set_2.issuperset(set_1)
